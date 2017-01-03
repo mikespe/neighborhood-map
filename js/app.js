1f8ae8fb53eb877ea9a2f74e6a7f55f40cc861a9
mikespe <msperan1@ramapo.edu>
2017-01-02 21:15:50 -0500

    added core files

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var locations = [
+  //initial location names and coordinates
+  {
+    name: 'scoopy',
+    lat: '70',
+    lng: '-120'
+  },
+  {
+    name: 'scoopy',
+    lat: '75',
+    lng: '-115'
+  },
+  {
+    name: 'scoopy',
+    lat: '73',
+    lng: '-123'
+  },
+  {
+    name: 'scoopy',
+    lat: '77',
+    lng: '-111'
+  },
+  {
+    name: 'scoopy',
+    lat: '80',
+    lng: '-110'
+  },
+];
